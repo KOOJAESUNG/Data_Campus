534f5644f727e4c9f73aef4b5615254610e9993e
구재성 <KOOJAESUNG>
2022-08-17 00:28:16 +0900

    .

--- 전처리_정규화__1인당자가용_대졸자비율/대졸자비율.py
+++ 전처리_정규화__1인당자가용_대졸자비율/대졸자비율.py
@@ -16,14 +16,14 @@ for i in list(univ.index):
     if type(i)==str:
         gu.append(i)
 univ_gu=pd.DataFrame(univ_sum,gu)
-univ_gu.columns=['서울']
+univ_gu.columns=['대졸이상비율']
 univ_gu.index.name='2020'
 univ_gu
 
 people=pd.read_excel("전처리_정규화__1인당자가용_대졸자비율/주민등록인구(연령별_동별)_20220816173236.xlsx",index_col='동별(2)')
 people.drop(index=['동별(2)','소계'],columns=['동별(1)','항목'],inplace=True)
 people.index.name='2020'
-people.columns=['서울']
+people.columns=['대졸이상비율']
 people_gu=people.astype(int)
 
 univ_per_people=univ_gu/people_gu
