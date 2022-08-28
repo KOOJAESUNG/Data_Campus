0bfe0e54a9095cc4e1b65669a788755c086748b9
구재성 <KOOJAESUNG>
2022-08-29 02:13:45 +0900

    .

--- 전처리_정규화__1인당자가용_대졸자비율/1인당자가용.py
+++ 전처리_정규화__1인당자가용_대졸자비율/1인당자가용.py
@@ -1,20 +1,27 @@
 import pandas as pd
 
-car=pd.read_excel("전처리_정규화__1인당자가용_대졸자비율/자동차등록(월별_구별)_20220816172135.xlsx",index_col='자치구별(2)')
+#서울 구별 자동차등록 수 데이터를 추출한다.
+car=pd.read_excel("./자동차등록(월별_구별)_20220816172135.xlsx",index_col='자치구별(2)')
 car=pd.DataFrame(car.iloc[:,1])
 car.drop(index='자치구별(2)',inplace=True)
 car.columns=['서울']
 car.index.name='2020'
 car=car.astype(int)
 
-people=pd.read_excel("전처리_정규화__1인당자가용_대졸자비율/주민등록인구(연령별_동별)_20220816173236.xlsx",index_col='동별(2)')
+#서울 구별 주민등록인구수를 추출한다.
+people=pd.read_excel("./주민등록인구(연령별_동별)_20220816173236.xlsx",index_col='동별(2)')
+people.index
 people.drop(index='동별(2)',columns=['동별(1)','항목'],inplace=True)
 people.index.name='2020'
 people.columns=['서울']
 people=people.astype(int)
+
+#서울 구별 1인당 자가용 수를 구한다.
 car_per_people=car/people
 car_per_people.columns=['1인당 자가용 수']
 car_per_people.drop(index='소계',inplace=True)
 car_per_people.describe()
+
+#서울 구별 1인당 자가용 수를 정규화한다.
 car_per_people['정규화']=(car_per_people-car_per_people.min())/(car_per_people.max()-car_per_people.min())
-car_per_people.to_excel("전처리_정규화__1인당자가용_대졸자비율/1인당 자가용 수.xlsx")
+car_per_people.to_excel("./1인당 자가용 수.xlsx")
--- 전처리_정규화__1인당자가용_대졸자비율/대졸자비율.py
+++ 전처리_정규화__1인당자가용_대졸자비율/대졸자비율.py
@@ -1,32 +1,39 @@
 import pandas as pd
 
-univ=pd.read_excel("전처리_정규화__1인당자가용_대졸자비율/교육정도별+인구(6세+이상)_계.xlsx",index_col='자치구별(2)')
+#서울 구별 대졸자 수를 구한다
+univ=pd.read_excel("./교육정도별+인구(6세+이상)_계.xlsx",index_col='자치구별(2)')
 univ.drop(columns=['자치구별(1)','교육정도별(1)','연령별(1)'],inplace=True)
 univ
 univ=univ.loc['종로구':,'2020':]
 univ.index.name='2020'
 univ
 univ_sum=[]
+#대학,대학교,대학원 졸업자 수를 합산한다.
 for i in range(2,univ['2020'].count(),3):
     univ_sum.append(int(univ.iloc[i,]+univ.iloc[i-1,]+univ.iloc[i-2,]))
 len(univ_sum)
 
+#인덱스에서 nan을 제외하고 gu 리스트에 넣는다.
 gu=[]
 for i in list(univ.index):
     if type(i)==str:
         gu.append(i)
+#서울 구별 대졸이상인구수 데이터프레임을 만든다.
 univ_gu=pd.DataFrame(univ_sum,gu)
-univ_gu.columns=['대졸이상비율']
+univ_gu.columns=['대졸이상비율']#데이터프레임끼리 계산을 위해 컬럼명 변경
 univ_gu.index.name='2020'
 univ_gu
 
-people=pd.read_excel("전처리_정규화__1인당자가용_대졸자비율/주민등록인구(연령별_동별)_20220816173236.xlsx",index_col='동별(2)')
+#서울 구별 주민등록인구를 추출한다.
+people=pd.read_excel("./주민등록인구(연령별_동별)_20220816173236.xlsx",index_col='동별(2)')
 people.drop(index=['동별(2)','소계'],columns=['동별(1)','항목'],inplace=True)
 people.index.name='2020'
-people.columns=['대졸이상비율']
+people.columns=['대졸이상비율'] #데이터프레임끼리 계산을 위해 컬럼명 변경
 people_gu=people.astype(int)
 
+#서울 구별 대졸자 비율을 구한다.
 univ_per_people=univ_gu/people_gu
+
+#서울 구별 대졸자 비율을 정규화한다.
 univ_per_people['정규화']=(univ_per_people-univ_per_people.min())/(univ_per_people.max()-univ_per_people.min())
-univ_per_people.to_excel("전처리_정규화__1인당자가용_대졸자비율/대졸이상비율.xlsx")
-univ_per_people.sort_values('정규화')
+univ_per_people.to_excel("./대졸이상비율.xlsx")
